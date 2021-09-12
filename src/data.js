a15720759445dceaa94b4198121ddd389773c0cd
xtremeevan <80179007+xtremeevan@users.noreply.github.com>
2021-09-12 18:03:21 -0500

    first commit

---
+++ src/data.js
@@ -0,0 +1,23 @@
+const data = {
+    products: [
+        {
+        id: '1', 
+        name: 'Macbook',
+        price: 1400,
+        image: 'https://images.pexels.com/photos/5231332/pexels-photo-5231332.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+    },
+    {
+        id: '2', 
+        name: 'Beach trip',
+        price: 280,
+        image: 'https://picsum.photos/id/100/367/267',
+    },
+    {
+        id: '3', 
+        name: 'Mens Leather Jacket',
+        price: 30,
+        image: 'https://picsum.photos/id/1005/367/267',
+    },
+    ],
+};
+export default data;
